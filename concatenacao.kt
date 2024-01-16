29a7de025bd656504fe2999da6363451e748553d
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-16 09:59:09 -0300

    To Save 16/01/2024
    concatenation

--- concatenacao.kt
+++ concatenacao.kt
@@ -2,7 +2,11 @@ fun main(){
     var nome = "Maria"
     var idade = 25
     var profissao = "Progamadora web"
+    val a = 10
+    val b = 5
+    val resultado = "A soma de $a e $b é ${a + b}"
 
     println("Meu nome e " + nome + " eu tenho " + idade + " anos e sou " + profissao)
     println("Meu nome e $nome eu tenho $idade anos e sou $profissao")
+    println(resultado)
 }
