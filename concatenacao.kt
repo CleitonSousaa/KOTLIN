df660908c192344f5ff3eed50391a88154e4c03b
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-16 08:52:21 -0300

    16/01/2024
    types of concatenation

---
+++ concatenacao.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var nome = "Maria"
+    var idade = 25
+    var profissao = "Progamadora web"
+
+    println("Meu nome e " + nome + " eu tenho " + idade + " anos e sou " + profissao)
+    println("Meu nome e $nome eu tenho $idade anos e sou $profissao")
+}
