3542782006c7e5621abab9c404ee2f53da9d0973
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-16 08:58:36 -0300

    To Save 16/01/2024
    Arithmetic Operators

---
+++ operadores.kt
@@ -0,0 +1,35 @@
+fun main(){
+    // Adição
+    val soma = 5 + 3
+    println("Adição: $soma")
+
+    // Subtração
+    val subtracao = 7 - 2
+    println("Subtração: $subtracao")
+
+    // Multiplicação
+    val multiplicacao = 4 * 6
+    println("Multiplicação: $multiplicacao")
+
+    // Divisão
+    val divisao = 10 / 2
+    println("Divisão: $divisao")
+
+    // Módulo
+    val modulo = 15 % 4
+    println("Módulo: $modulo")
+
+    // Incremento e Decremento
+    var numero = 5
+    numero++
+    println("Incremento: $numero")
+
+    numero--
+    println("Decremento: $numero")
+
+    // Operadores de atribuição combinada
+    var x = 10
+    x += 5
+    println("Operador de atribuição combinada: $x")
+
+}
