bbcd0fca0ae92388464f7053e92389b6164150f3
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-16 09:42:58 -0300

    To Save 16/01/2024
    Arrays

---
+++ arrays.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(){
+    var listaCompras = arrayOf("Arroz", "Feijão", "Batata", "Uva", "Banana", "Carne")
+
+    // acessar por indice
+    println(listaCompras[2])
+
+    // acessar array completo
+    println(Arrays.toString(listaCompras))
+
+    // tamanho da lista
+    println(listaCompras.size)
+}
