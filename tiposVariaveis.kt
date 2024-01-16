81b8dbd9af046236ac53e1c3405baf37829bddbc
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-01-16 08:40:06 -0300

    To Save 16/01/2024
    variable types

---
+++ tiposVariaveis.kt
@@ -0,0 +1,24 @@
+fun main(){
+    /*
+    String - textos
+    byte - números inteiros (-128 até 127)
+    int - números inteiros (-2147483648 até 2147483647)
+    long - números inteiros (-9223372036854775808 até 9223372036854775807)
+    float - números Decimais (1.1234567 até 7 casas decimais)
+    double - números Decimais (1.123456789123456 até 15 casas decimais)
+    boolean - booleanos
+    */
+
+    val nome: String = "Pedro"
+    val idade: Byte = 127
+    val saldo = 1.1234567f
+    val novoSaldo = 1.123456789012345
+    val bool = true
+  
+    println(nome)
+    println(idade)
+    println(saldo)
+    println(novoSaldo)
+    println(bool)
+    
+} 
